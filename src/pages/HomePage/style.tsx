668e4ae6e0430e994c422323a4100b8704c9b542
renanribeir <renanribeirow19@gmail.com>
2021-12-26 20:14:07 -0300

    style: Homepage navbar

--- src/pages/HomePage/style.tsx
+++ src/pages/HomePage/style.tsx
@@ -10,10 +10,12 @@ export default StyleSheet.create({
   navbar: {
     backgroundColor: '#000',
     height: 40,
-    width: '100%',
+    width: '90%',
     justifyContent: 'center',
     alignItems: 'center',
-  },
+    marginTop: 10,	  
+    borderRadius: 15, 
+},
   text: {
     color: '#fff',
   },
